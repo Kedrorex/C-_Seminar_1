63365f2b20479c5269fdb0fa8831eb90fdf8edb0
Kedrorex <Belousovvk@bk.ru>
2023-02-10 22:24:31 +0400

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+  case 1:
+  Console.Write("Понедельник");
+  break;
+
+  case 2:
+  Console.Write("Вторник");
+  break;
+
+  case 3:
+  Console.Write("Среда");
+  break;
+
+  case 4:
+  Console.Write("Четверг");
+  break;
+
+  case 5:
+  Console.Write("Пятница");
+  break;
+
+  case 6:
+  Console.Write("Суббота");
+  break;
+
+  case 7:
+  Console.Write("Воскресенье");
+  break;
+
+  default:
+  Console.Write("Не верное значение!");
+  break;
+}
