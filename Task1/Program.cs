ef075a7a0cd8d3c45e0323664d29b80d5e8d4933
Kedrorex <Belousovvk@bk.ru>
2023-02-10 22:14:21 +0400

    Added koren

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int squere = 0;
+squere = number * number;
+
+Console.Write($"Квадрат числа {number} = {squere}");
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число Б: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int sqwere = numberA / numberB;
+
+if (sqwere == numberB)
+{
+  Console.Write($"Число {numberB} является корнем {numberA}");
+}
+else 
+{
+  Console.Write($"Число {numberB} не является корнем {numberA}");
+}
